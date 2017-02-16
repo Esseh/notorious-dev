e0f059c5501fae25c0f527f07fe607cce05e0007
Esseh <kennethwilleford@gmail.com>
2017-02-15 18:03:14 -0800

    Updated CONTEXT

--- CONTEXT/CONTEXT_Structure.go
+++ CONTEXT/CONTEXT_Structure.go
@@ -23,10 +23,10 @@ type HeaderData struct {
 func NewContext(res http.ResponseWriter, req *http.Request) Context{
 	user, err := USERS.GetUserFromSession(req)
 	ctx := Context { 
-		req: req,
-		res: res,
-		user: user,
-		userException: err,
+		Req: req,
+		Res: res,
+		User: user,
+		UserException: err,
 	}
 	ctx.Context = appengine.NewContext(req)
 	return ctx
@@ -36,29 +36,29 @@ func NewContext(res http.ResponseWriter, req *http.Request) Context{
 // and stores useful metadata.
 type Context struct {
 	// The active request 
-	req *http.Request
+	Req *http.Request
 	// The output writer to the user's browser.
-	res http.ResponseWriter
+	Res http.ResponseWriter
 	// The currently logged in user.
-	user *USERS.User
+	User *USERS.User
 	// Any problems that occured while logging in.
-	userException error
+	UserException error
 	// transaction timing information.
 	context.Context
 }
 
 // Returns true if the user is not logged in.
 func (ctx Context)AssertLoggedInFailed() bool {
-	if ctx.userException != nil {
-		path := strings.Replace(ctx.req.URL.Path[1:], "%2f", "/", -1)
-		http.Redirect(ctx.res, ctx.req, PATHS.AUTH_Login+"?redirect="+path, http.StatusSeeOther)
+	if ctx.UserException != nil {
+		path := strings.Replace(ctx.Req.URL.Path[1:], "%2f", "/", -1)
+		http.Redirect(ctx.Res, ctx.Req, PATHS.AUTH_Login+"?redirect="+path, http.StatusSeeOther)
 		return true
 	}
 	return false
 }
 
 // Simplified redirect, useful for general redirects. If the redirect demands a more severe status code use tradition http.Redirect.
-func (ctx Context)Redirect(uri string){ http.Redirect(ctx.res, ctx.req, uri, http.StatusSeeOther) }
+func (ctx Context)Redirect(uri string){ http.Redirect(ctx.Res, ctx.Req, uri, http.StatusSeeOther) }
 
 
 /// Prints an error page to response and returns a boolean representation of the function executing.
@@ -68,8 +68,8 @@ func (ctx Context)Redirect(uri string){ http.Redirect(ctx.res, ctx.req, uri, htt
 /// Usage: Use if there is no constructive alternative.
 func (ctx Context)ErrorPage(ErrorTitle string, e error, errCode int) bool {
 	if e != nil {
-		if ctx.user == nil {
-			ctx.user = &USERS.User{}
+		if ctx.User == nil {
+			ctx.User = &USERS.User{}
 		}
 		args := &struct {
 			Header    HeaderData
@@ -77,10 +77,10 @@ func (ctx Context)ErrorPage(ErrorTitle string, e error, errCode int) bool {
 			ErrorDump error
 			ErrorCode int
 		}{
-			HeaderData{ctx, ctx.user, ""}, ErrorTitle, e, errCode,
+			HeaderData{ctx, ctx.User, ""}, ErrorTitle, e, errCode,
 		}
-		ctx.res.WriteHeader(errCode)
-		CORE.ServeTemplateWithParams(ctx.res, "site-error", args)
+		ctx.Res.WriteHeader(errCode)
+		CORE.ServeTemplateWithParams(ctx.Res, "site-error", args)
 		return true
 	}
 	return false
@@ -93,12 +93,12 @@ func (ctx Context)ErrorPage(ErrorTitle string, e error, errCode int) bool {
 /// Usage: Use in POST calls accessed from a GET of the same handle.
 func (ctx Context)BackWithError(err error, errorString string) bool {
 	if err != nil {
-		path := strings.Replace(ctx.req.URL.Path, "%2f", "/", -1)
+		path := strings.Replace(ctx.Req.URL.Path, "%2f", "/", -1)
 		path += "?"+url.QueryEscape("ErrorResponse")+"="+url.QueryEscape(errorString)
-		if ctx.req.FormValue("redirect") != "" {
-			path += "&"+url.QueryEscape("redirect")+"="+ctx.req.FormValue("redirect")		
+		if ctx.Req.FormValue("redirect") != "" {
+			path += "&"+url.QueryEscape("redirect")+"="+ctx.Req.FormValue("redirect")		
 		}
-		http.Redirect(ctx.res, ctx.req, path, http.StatusSeeOther)
+		http.Redirect(ctx.Res, ctx.Req, path, http.StatusSeeOther)
 		return true
 	}
 	return false
