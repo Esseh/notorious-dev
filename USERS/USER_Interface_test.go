4ef85d9cc5c294eba1ac2bb25f8155ab1c564cb8
Esseh <kennethwilleford@gmail.com>
2017-02-16 17:02:05 -0800

    USERS Structure UT Done

--- USERS/USER_Interface_test.go
+++ USERS/USER_Interface_test.go
@@ -1,35 +1,17 @@
 package USERS
-
-import (
-	"image"
-	_ "image/jpeg"
-	_ "image/png"
-	"io"
-	"net/http"
-	"time"
-	"strconv"
-	"mime/multipart"
-	"errors"
-	"google.golang.org/appengine"
-	"github.com/Esseh/retrievable"
-	"golang.org/x/net/context"
-	"github.com/Esseh/notorious-dev/CORE"
-	"github.com/Esseh/notorious-dev/COOKIE"
-	"github.com/Esseh/notorious-dev/CLOUD"
-)
-
+/*
 
 // Uploads an avatar for a user into the cloud.
 func UploadAvatar(ctx context.Context, userID int64, header *multipart.FileHeader, avatarReader io.ReadSeeker) error {
-	m, _, err := image.Decode(avatarReader)
-	if err != nil { return err }
+	m, _, err := image.Decode(avatarReader)				
+	if err != nil { return err }				// Bad Data Path
 	imageBounds := m.Bounds()
-	if imageBounds.Dy() > CORE.MaxAvatarSize || imageBounds.Dx() > CORE.MaxAvatarSize {
+	if imageBounds.Dy() > CORE.MaxAvatarSize || imageBounds.Dx() > CORE.MaxAvatarSize {		// Unsatisfied Constraint Path
 		return errors.New("Filesize is Too Large")
 	}
 	avatarReader.Seek(0, 0)
 	filename := CORE.GetAvatarPath(userID)
-	return CLOUD.AddFile(ctx, filename, header.Header["Content-Type"][0], avatarReader)
+	return CLOUD.AddFile(ctx, filename, header.Header["Content-Type"][0], avatarReader)		// end of normal path
 }
 
 
@@ -37,14 +19,14 @@ func UploadAvatar(ctx context.Context, userID int64, header *multipart.FileHeade
 // Retrieves an AUTH_User from the currently logged in user.
 func GetUserFromSession(req *http.Request) (*User, error) {
 	userID, err := GetUserIDFromRequest(req)
-	if err != nil { return &User{}, err }
+	if err != nil { return &User{}, err }			// user could not be retrieved path
 	ctx := appengine.NewContext(req)
-	return GetUserFromID(ctx, userID)
+	return GetUserFromID(ctx, userID)				// end of normal path, needs stubbed database entry
 }
 
 // Retrieves an AUTH_User ID from the currently logged in user.
 func GetUserIDFromRequest(req *http.Request) (int64, error) {
-	s, err := GetSessionID(req)
+	s, err := GetSessionID(req)					// BAD Session ID in Cookie
 	if err != nil { return 0, err }
 	ctx := appengine.NewContext(req)
 	userID, err := GetUserIDFromSession(ctx, s)
@@ -78,9 +60,10 @@ func GetSession(ctx context.Context, sessionID int64) (Session, error) {
 
 // Retrieves a Session ID from the currently logged in user.
 func GetSessionID(req *http.Request) (int64, error) {
-	sessionIDStr, err := COOKIE.GetValue(req, "session")
+	sessionIDStr, err := COOKIE.GetValue(req, "session")			// Bad Stored Form
 	if err != nil { return -1, errors.New("Not Logged In") }
 	id, err := strconv.ParseInt(sessionIDStr, 10, 64) // Change cookie val into key
-	if err != nil { return -1, errors.New("Invalid Logged In") }
-	return id, nil
+	if err != nil { return -1, errors.New("Invalid Logged In") }	// Bad Final Key Form
+	return id, nil													// End of Normal Path
 }
+*/
