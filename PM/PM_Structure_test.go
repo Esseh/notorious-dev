782cb120c38de7b21187a6fef284b0e8e57e265a
Esseh <kennethwilleford@gmail.com>
2017-04-13 19:07:34 -0700

    Implemented Structure Test

---
+++ PM/PM_Structure_test.go
@@ -0,0 +1,19 @@
+package PM
+
+import(
+	"fmt"
+	"testing"
+	"google.golang.org/appengine/aetest"
+)
+
+func TestMessageAndHeader(t *testing.T){
+	ctx, done, err := aetest.NewContext()
+	defer done()
+	if err != nil {
+		fmt.Println("PANIC in TestFolder")
+		panic(1)
+	}
+	// No assertion to check, if it compiles it fulfills the interface.
+	(&PrivateMessage{}).Key(ctx,int64)
+	(&PrivateMessageHeader{}).Key(ctx,int64)
+}
